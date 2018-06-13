c747e41e0a101a958b1311983bae07305a07c98f
lhuynh27 <leannehuynh27@gmail.com>
2018-06-13 15:40:23 -0700

    Updated .push, .pop, .size to pass all test

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,20 @@
 var Stack = function() {
   var someInstance = {};
-
-  // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {}; // Use an object with numeric keys to store values
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
-  };
+    var deletedLast = storage[someInstance.size() - 1]
+    delete storage[someInstance.size() - 1];
+    return deletedLast;
+  };  
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
