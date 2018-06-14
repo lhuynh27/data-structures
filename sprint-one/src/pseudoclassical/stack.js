93f0aab762fe3bd2e84c6dca27abff375f205e45
lhuynh27 <leannehuynh27@gmail.com>
2018-06-14 00:59:18 -0700

    Code follows pseudoclassical guildelines

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  return this.count;
 };
 
+Stack.prototype.push = function(value) {
+    this[this.count] = value;
+    this.count++;
+  };
 
+Stack.prototype.pop = function() {
+ 	if (this.count > 0){
+	    var deletedLast = this[this.count - 1];
+    	delete deletedLast;
+    	this.count--;
+    	return deletedLast;
+    }	
+  };
+
+Stack.prototype.size = function() {
+    return this.count;
+  };
