7661ad6a55db89a1547ff414e9ba53c745ae74e0
lhuynh27 <leannehuynh27@gmail.com>
2018-06-14 00:21:38 -0700

    Made var newQueue = Object.create instead of _.extend

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.front = 0;
+  newQueue.end = 0;
+
+  return newQueue;
+  };
+
+var queueMethods = {
+	enqueue: function(value) {
+    this[this.end] = value;
+    this.end++;
+  },
+
+ 	dequeue: function() {	
+ 	if ((this.end - this.front) > 0){
+	    var deletedFirst = this[this.front];
+    	delete this[this.front];
+    	this.front++;
+    	return deletedFirst;
+    }
+  },
+
+  	size: function() {
+    return this.end - this.front;
+  }
 };
-
-var queueMethods = {};
-
-
