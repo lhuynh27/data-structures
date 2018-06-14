02b70121e6f51fdca3485497dfb3529567d32a37
lhuynh27 <leannehuynh27@gmail.com>
2018-06-14 00:55:10 -0700

    change code in order to use constructors

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.front = 0;
+  	this.end = 0;
+  }
+
+  enqueue(value) {
+    this[this.end] = value;
+    this.end++;
+  }
+
+  dequeue() {	
+ 	if ((this.end - this.front) > 0){
+	    var deletedFirst = this[this.front];
+    	delete this[this.front];
+    	this.front++;
+    	return deletedFirst;
+    }
+  }
+
+  size() {
+    return this.end - this.front;
   }
 
 }
