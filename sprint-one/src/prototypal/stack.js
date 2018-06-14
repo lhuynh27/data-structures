f8de5cd50569e4be1ffd3ec462add5dea0753a8c
lhuynh27 <leannehuynh27@gmail.com>
2018-06-14 00:22:28 -0700

    Used Object.create instead of _.extend for queueMethods

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods)
+  newStack.count = 0;
+  return newStack;
+  };
+
+var stackMethods = {
+	push: function(value) {
+    this[this.count] = value;
+    this.count++;
+  },
+
+ 	pop: function() {
+ 	if (this.count > 0){
+	    var deletedLast = this[this.count - 1];
+    	delete deletedLast;
+    	this.count--;
+    	return deletedLast;
+    }	
+  },
+
+  	size: function() {
+    return this.count;
+  }
 };
-
-var stackMethods = {};
-
-
