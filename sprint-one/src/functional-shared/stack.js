8d4c7a5a38cd545ad271e5a9965131a0121975d5
lhuynh27 <leannehuynh27@gmail.com>
2018-06-14 00:57:06 -0700

    Modified code in order to follow the guildlines of shared methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.count = 0;
+  _.extend(newStack, stackMethods)
+  return newStack;
+  };
+
+var stackMethods = {
+	push: function(value) {
+    this[this.count] = value;
+    this.count++;
+  },
+
+ 	pop: function() {
+ 	if (this.count > 0){
+	    var deletedLast = this[this.count - 1];
+    	delete deletedLast;
+    	this.count--;
+    	return deletedLast;
+    }	
+  },
+
+  	size: function() {
+    return this.count;
+  }
 };
-
-var stackMethods = {};
-
-
