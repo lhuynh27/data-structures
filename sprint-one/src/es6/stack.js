d199b9f1312e490bca0eb1f3101f19ea59a9d3fa
lhuynh27 <leannehuynh27@gmail.com>
2018-06-14 00:55:56 -0700

    Stack now uses constructor

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,25 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
-  }
+	constructor() {
+  		this.count = 0;
+  	}	
 
-}
+	push(value) {
+   		this[this.count] = value;
+    	this.count++;
+	}
+
+	pop() {
+		if (this.count > 0){
+ 			var deletedLast = this[this.count - 1];
+    		delete this[this.count - 1];
+    		this.count--;
+    		return deletedLast;
+ 		}	
+	}  
+
+	size() {
+    	return this.count;
+	}
+}	
